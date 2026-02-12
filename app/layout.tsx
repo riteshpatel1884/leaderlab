8501bded12208eacee1e88ab91d59bbec9a0e1e1
riteshpatel-1884 <riteshpatel1882004@gmail.com>
2026-02-12 22:52:22 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -47,6 +47,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
